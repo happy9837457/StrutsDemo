fab0b4d6a6b9418f378981704e5a73e8f984de92
happy9837457 <happy9837457@163.com>
2014-08-28 16:05:26 +0800

    UserInfo commit

--- src/com/palm/struts/bean/UserInfo.java
+++ src/com/palm/struts/bean/UserInfo.java
@@ -1,9 +1,47 @@
 package com.palm.struts.bean;
+
+import java.util.Date;
+
 /**
  * 
  * @author weixiang.qin
  *
  */
 public class UserInfo {
+	private int userId;
+	private String username;
+	private String password;
+	private Date createTime;
 
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
